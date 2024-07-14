e273435148780af39ad927be4599e4c1da723473
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-14 11:32:28 +0330

    add skills 1/3

--- app/_components/skills.tsx
+++ app/_components/skills.tsx
@@ -1,31 +1,21 @@
-"use client";
-import { animated, useSpring } from "@react-spring/web";
-import { useRef } from "react";
+'use client';
 import {
-  CssIcon,
-  NestIcon,
-  LaravelIcon,
-  NuxtIcon,
-  WordpressIcon,
-  PrismaIcon,
-  TRPCIcon,
-  NodeIcon,
+  CSSIcon,
   GithubIcon,
-  PythonIcon,
+  JavaScriptIcon,
+  NextjsIcon,
+  NodeIcon,
+  PostgreSQLIcon,
+  PrismaIcon,
   ReactIcon,
-  TypeScriptIcon,
-  SassIcon,
   ReduxIcon,
-  VueIcon,
-  JsIcon,
-  TailwindIcon,
-} from "~/assets/icons";
-import useIntersectionObserver from "~/lib/hooks/useIntersectionObserver";
-import styles from "~/styles/modules/skills.module.css";
-
-interface SkillsProps {
-  title: string;
-}
+  SassIcon,
+  TailwindCSSIcon,
+  TypeScriptIcon,
+  WordPressIcon,
+} from '@/components/icons';
+import { motion } from 'framer-motion';
+import { ArrowRight } from 'lucide-react';
 
 interface SkillProps {
   icon: any;
@@ -36,133 +26,98 @@ interface SkillProps {
 const skills: SkillProps[] = [
   {
     icon: ReactIcon,
-    title: "React",
-    level: "advanced",
+    title: 'React.JS',
+    level: 'advanced',
   },
   {
     icon: TypeScriptIcon,
-    title: "TypeScript",
-    level: "advanced",
+    title: 'TypeScript',
+    level: 'advanced',
   },
   {
-    icon: JsIcon,
-    title: "JavaScript ES2023",
-    level: "advanced",
+    icon: NextjsIcon,
+    title: 'Next.JS',
+    level: 'advanced',
   },
   {
-    icon: TailwindIcon,
-    title: "Tailwind",
-    level: "advanced",
+    icon: JavaScriptIcon,
+    title: 'JavaScript ES2023',
+    level: 'advanced',
   },
   {
-    icon: CssIcon,
-    title: "CSS3",
-    level: "advanced",
+    icon: TailwindCSSIcon,
+    title: 'Tailwind',
+    level: 'advanced',
+  },
+  {
+    icon: CSSIcon,
+    title: 'CSS3',
+    level: 'advanced',
   },
   {
     icon: SassIcon,
-    title: "Sass",
-    level: "advanced",
+    title: 'Sass',
+    level: 'advanced',
   },
   {
     icon: ReduxIcon,
-    title: "Redux",
-    level: "medium",
+    title: 'Redux',
+    level: 'medium',
   },
   {
     icon: NodeIcon,
-    title: "Node.JS",
-    level: "medium",
+    title: 'Node.JS',
+    level: 'medium',
   },
   {
-    icon: NestIcon,
-    title: "Nest.JS",
-    level: "advanced",
-  },
-  {
-    icon: TRPCIcon,
-    title: "tRPC",
-    level: "advanced",
+    icon: ReactIcon,
+    title: 'Nest.JS',
+    level: 'advanced',
   },
   {
     icon: PrismaIcon,
-    title: "Prisma",
-    level: "advanced",
+    title: 'Prisma',
+    level: 'advanced',
   },
   {
     icon: GithubIcon,
-    title: "Github",
-    level: "advanced",
+    title: 'Github',
+    level: 'advanced',
   },
   {
-    icon: LaravelIcon,
-    title: "Laravel",
-    level: "advanced",
+    icon: WordPressIcon,
+    title: 'WordPress',
+    level: 'advanced',
   },
   {
-    icon: WordpressIcon,
-    title: "WordPress",
-    level: "advanced",
-  },
-  {
-    icon: VueIcon,
-    title: "Vue.JS",
-    level: "medium",
-  },
-  {
-    icon: NuxtIcon,
-    title: "Nuxt",
-    level: "medium",
-  },
-  {
-    icon: PythonIcon,
-    title: "Python",
-    level: "medium",
+    icon: PostgreSQLIcon,
+    title: 'PostgreSQL',
+    level: 'advanced',
   },
 ];
 
-export default function Skills({ title }: SkillsProps) {
-  const triggerRef = useRef<any>(null);
-  const dataRef = useIntersectionObserver(triggerRef, {
-    freezeOnceVisible: false,
-  });
-  const itemProps = useSpring({
-    from: { y: 50, opacity: 0 },
-    to: {
-      y: dataRef?.isIntersecting ? 0 : 50,
-      opacity: dataRef?.isIntersecting ? 1 : 0,
-    },
-    delay: 300,
-    config: {
-      duration: 600,
-    },
-  });
-
+export function Skills() {
   return (
-    <div id="skills" className={`${styles.container}`} ref={triggerRef}>
-      <animated.h2 style={itemProps}>{title}</animated.h2>
-      <ul className={styles.items}>
-        {skills.map((item, i) => {
-          const itemProps = useSpring({
-            from: { opacity: 0 },
-            to: {
-              opacity: dataRef?.isIntersecting ? 1 : 0,
-            },
-            delay: i * 100,
-            config: {
-              duration: 1000,
-            },
-          });
-          return (
-            <animated.li key={item.title} style={itemProps}>
-              <item.icon />
-              <div className="flex items-center gap-2">
-                <span className="text-heading">{item.title}</span>
-                <span className="text-sm text-body">{item.level}</span>
-              </div>
-            </animated.li>
-          );
-        })}
+    <div id="skills">
+      <h2 className="mb-10 font-semibold text-2xl text-heading">Skills</h2>
+      <ul className={'flex flex-wrap items-center gap-8'}>
+        {skills.map((item, i) => (
+          <motion.li
+            key={item.title}
+            initial={{ opacity: 0, x: -15 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ delay: i * 0.1, duration: 0.5, type: 'spring' }}
+            className="flex items-center gap-3 text-lg"
+          >
+            <item.icon className="h-6 w-6" />
+            <div className="flex items-center gap-2">
+              <span className="font-medium text-sm">{item.title}</span>
+              <span className="text-muted-foreground text-xs">
+                {item.level}
+              </span>
+            </div>
+          </motion.li>
+        ))}
       </ul>
     </div>
   );
