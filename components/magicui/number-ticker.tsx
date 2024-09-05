77c796fd42874d99cd50aaa921e1810700445f6b
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-09-05 21:21:43 +0330

    remove expriense section

--- components/magicui/number-ticker.tsx
+++ components/magicui/number-ticker.tsx
@@ -1,41 +1,41 @@
-"use client";
+'use client';
 
-import { useEffect, useRef } from "react";
-import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useInView, useMotionValue, useSpring } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 export default function NumberTicker({
   value,
-  direction = "up",
+  direction = 'up',
   delay = 0,
   className,
 }: {
   value: number;
-  direction?: "up" | "down";
+  direction?: 'up' | 'down';
   className?: string;
   delay?: number; // delay in s
 }) {
   const ref = useRef<HTMLSpanElement>(null);
-  const motionValue = useMotionValue(direction === "down" ? value : 0);
+  const motionValue = useMotionValue(direction === 'down' ? value : 0);
   const springValue = useSpring(motionValue, {
     damping: 60,
     stiffness: 100,
   });
-  const isInView = useInView(ref, { once: true, margin: "0px" });
+  const isInView = useInView(ref, { once: true, margin: '0px' });
 
   useEffect(() => {
     isInView &&
       setTimeout(() => {
-        motionValue.set(direction === "down" ? 0 : value);
+        motionValue.set(direction === 'down' ? 0 : value);
       }, delay * 1000);
   }, [motionValue, isInView, delay, value, direction]);
 
   useEffect(
     () =>
-      springValue.on("change", (latest) => {
+      springValue.on('change', (latest) => {
         if (ref.current) {
-          ref.current.textContent = Intl.NumberFormat("en-US").format(
+          ref.current.textContent = Intl.NumberFormat('en-US').format(
             latest.toFixed(0),
           );
         }
@@ -46,7 +46,7 @@ export default function NumberTicker({
   return (
     <span
       className={cn(
-        "inline-block tabular-nums text-black dark:text-white tracking-wider",
+        'inline-block text-black tabular-nums tracking-wider dark:text-white',
         className,
       )}
       ref={ref}
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
