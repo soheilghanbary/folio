a9b976d9aeb02f7f3742724e5360acdca6f2b163
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-09-06 15:20:49 +0330

    update projects local

--- app/_components/projects.tsx
+++ app/_components/projects.tsx
@@ -1,85 +1,12 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import { projects } from '@/data/projects';
 import { motion } from 'framer-motion';
 import { GithubIcon, LinkIcon } from 'lucide-react';
-import { useTranslations } from 'next-intl';
-
-const projects = [
-  {
-    name: 'ریپیتر من (MyRepeater)',
-    description: 'وبسایت تخصصی فروش تقویت آنتن موبایل',
-    title: 'SEO Engineer',
-    link: 'https://myrepeater.ir',
-    image: 'https://www.karlancer.com/api/file/x/x764/1723907835-WLfa.png',
-    tags: ['SEO', 'Yoast', 'WordPress'],
-  },
-  {
-    name: 'نوین بوستر (Novin Booster)',
-    description: 'تولید کننده تجهیزات مخابراتی و رادیویی',
-    title: 'WordPress Work',
-    link: 'https://myrepeater.ir',
-    image: 'https://www.karlancer.com/api/file/x/x764/1723908412-BkCo.jpg',
-    tags: ['SEO', 'RankMath', 'WordPress'],
-  },
-  {
-    name: 'وبکده (WebKade)',
-    description: 'پلتفرم اشتراک گذاری وب اپلیکیشن',
-    title: 'MERN Developer',
-    link: 'https://webkade.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908412-BkCo.jpg',
-    tags: ['React.JS', 'Express.Js', 'PostgreSQL'],
-  },
-  {
-    name: 'خرجینو (Kharjino)',
-    description: 'اپلیکیشن مدیریت محارج شخصی',
-    title: 'Next.JS Developer',
-    link: 'https://kharjino.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908412-BkCo.jpg',
-    tags: ['Next.JS', 'Prisma', 'PostgreSQL'],
-  },
-  {
-    name: 'شاپینو (Shopino)',
-    description: 'اپلیکیشن ثبت اگهی بدون محدودیت',
-    title: 'Next.JS Developer',
-    link: 'https://shopino.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908412-BkCo.jpg',
-    tags: ['Next.JS', 'DrizzleORM', 'PostgreSQL'],
-  },
-  {
-    name: 'کیان استور (KianStore)',
-    description: 'وب اپلیکیشن مدیریت انبارداری شرکت کیان ارتباط',
-    title: 'Full Stack Developer',
-    link: 'https://kianstore.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908412-BkCo.jpg',
-    tags: ['Next.JS', 'Prisma', 'MySQL', 'TypeScript'],
-  },
-  {
-    name: 'موویتو (Moovito)',
-    description: 'وب اپلیکیشن اطلاعات فیلم TMDB',
-    title: 'FrontEnd Developer',
-    link: 'https://moovito.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908412-BkCo.jpg',
-    tags: ['React.JS', 'TailwindCSS', 'Api'],
-  },
-  {
-    name: 'پستینو (Postino)',
-    description: 'نمونه شبکه اجتماعی نوشته با Next.JS',
-    title: 'Next.JS Developer',
-    link: 'https://postino.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908412-BkCo.jpg',
-    tags: ['Next.JS', 'Hono.JS', 'DrizzleORM'],
-  },
-  {
-    name: 'جابینو (Jobino)',
-    description: 'اپلیکیشن اشتراک گذاری مشاغل',
-    title: 'React.JS Developer',
-    link: 'https://jobino.vercel.app',
-    image: 'https://www.karlancer.com/api/file/x/x352_211/1723908660-y1Z9.jpg',
-    tags: ['Next.JS', 'Prisma', 'TypeScript'],
-  },
-];
+import { useLocale, useTranslations } from 'next-intl';
 
 export function Projects() {
+  const locale = useLocale();
   const t = useTranslations('projects');
 
   return (
@@ -94,7 +21,12 @@ export function Projects() {
       </motion.h2>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((p) => (
-          <ProjectItem key={p.name} {...p} />
+          <ProjectItem
+            key={p.name}
+            {...p}
+            name={locale === 'en' ? p.name : p.nameFa}
+            description={locale === 'en' ? p.description : p.descriptionFa}
+          />
         ))}
       </div>
       <Button
