7e5acd56f36592ce11011049537bd9683eef90d8
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-09-10 13:34:31 +0330

    update signature on dark mode

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,17 +1,25 @@
-import { useTranslations } from 'next-intl';
-import Image from 'next/image';
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 const Signature = () => (
-  <Image src="/signature.png" alt="Vercel" width={72} height={16} />
+  <Image
+    src="/signature.png"
+    alt="Vercel"
+    width={72}
+    height={16}
+    quality={100}
+    sizes="100vw"
+    className="dark:invert"
+  />
 );
 
 export function SiteFooter() {
-  const t = useTranslations('footer');
+  const t = useTranslations("footer");
   return (
-    <footer className="border-border/50 border-t bg-card dark:bg-background">
+    <footer className="border-t border-border/50 bg-card dark:bg-background">
       <section className="container mx-auto flex flex-col-reverse items-center justify-between gap-4 p-4 md:flex-row">
-        <p className="text-balance text-center text-foreground text-sm/6">
-          {t('description')}
+        <p className="text-balance text-center text-sm/6 text-foreground">
+          {t("description")}
         </p>
         <Signature />
       </section>
