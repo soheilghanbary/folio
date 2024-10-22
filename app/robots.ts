5c172b6b8ef4e825ec8e35c100745dbd33b43bbc
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-10-22 12:53:54 -0700

    update robots & sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://folio-steel.vercel.app/sitemap.xml',
+    sitemap: 'https://soheilghanbary.ir/sitemap.xml',
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://folio-steel.vercel.app',
+      url: 'https://soheilghanbary.ir',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
